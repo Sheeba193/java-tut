a7631f74ae5b342d1ee0e966459049e3438b5963
Sheeba193 <bethkerubo193@gmail.com>
2026-09-20 23:52:09 +0300

    hypotenuse of triangle using Math

---
+++ src/Mathproblems.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Mathproblems {
+//    public static void main (String[] args){
+//
+//        System.out.println(Math.PI);
+//
+//        double result;
+//        double absresult;
+//        double sqrresult;
+//
+//        result = Math.pow(2,3);
+//        absresult = Math.abs(-5);
+//        sqrresult = Math.sqrt(496);
+//
+//
+//        System.out.println(result);
+//        System.out.println(absresult);
+//        System.out.println(sqrresult);
+//
+//
+//    }
+    public static void main(String[] args){
+        //HYPOTENUSE OF A TRIANGLE = sqrt of the sum of the squares of the sides
+
+        Scanner scanner = new Scanner(System.in);
+
+        double a;
+        double b;
+        double c;
+
+
+        System.out.println("enter length of side a: ");
+        a = scanner.nextDouble();
+
+        System.out.println("enter length of side b: ");
+        b = scanner.nextDouble();
+
+        c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+
+        System.out.println("the hypotenuse length is " + c + " sqr units");
+        scanner.close();
+    }
+}
